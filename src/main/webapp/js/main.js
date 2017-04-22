0154c0bfe2c2f4082b9f888c6eb62165e6d4dd18
Alan O'Neill <alan.oneill75@gmail.com>
2017-04-22 18:53:58 +0100

    More and more

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -54,13 +54,9 @@ function showHide(shID) {
 	if (document.getElementById(shID)) {
 		if (document.getElementById(shID + '-show').style.display != 'none') {
 			document.getElementById(shID + '-show').style.display = 'none';
-			document.getElementById(shID).style.display = 'none';
-			document.getElementById(shID + '-s').style.display = 'block';
-			document.getElementById(shID + 'm').style.display = 'block';
+			document.getElementById(shID).style.display = 'block';
 		} else {
 			document.getElementById(shID + '-show').style.display = 'inline';
-			document.getElementById(shID).style.display = 'block';
-			document.getElementById(shID + '-s').style.display = 'none';
 			document.getElementById(shID).style.display = 'none';
 		}
 	}
