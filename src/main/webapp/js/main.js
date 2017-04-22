39e42a16f242401a5ff63d8c88059ee0773b9b21
Alan O'Neill <alan.oneill75@gmail.com>
2017-04-22 15:45:39 +0100

    Lots of redesign of colours and fonts

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(
+		function() {
+
+			// Variables
+			var $nav = $(".nav"), $window = $(window), $body = $("body"),
+			// returns the offset coordinates in px for the top element .nav
+			navOffsetTop = $nav.offset().top
+
+			function init() {
+				// Add event listeners on scroll and resize
+				$window.on('scroll', onScroll)
+				$window.on('resize', resize)
+			}
+
+			function resize() {
+				$body.removeClass('has-docked-nav')
+				navOffsetTop = $nav.offset().top
+				onScroll()
+			}
+
+			// $window.scrollTop(): Get the current vertical position of the
+			// scroll bar
+			// for the first element in the set of matched elements
+
+			function onScroll() {
+				if (navOffsetTop < $window.scrollTop()
+						&& !$body.hasClass('has-docked-nav')) {
+					$body.addClass('has-docked-nav')
+				}
+
+				if (navOffsetTop > $window.scrollTop()
+						&& $body.hasClass('has-docked-nav')) {
+					$body.removeClass('has-docked-nav')
+				}
+			}
+
+			init();
+
+		});
+
+function openNav() {
+	document.getElementById("sidenav").style.width = "14em";
+	document.getElementById("toggle").className = document
+			.getElementById("toggle").className.replace(/\btoggle\b/, '');
+};
+
+function closeNav() {
+	document.getElementById("sidenav").style.width = "0";
+	document.getElementById("toggle").className = document
+			.getElementById("toggle").className.replace('', 'toggle');
+};
