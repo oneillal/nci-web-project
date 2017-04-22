17be3b3826bc95bb7c2d663a37a2076bc87ef5bd
Alan O'Neill <alan.oneill75@gmail.com>
2017-04-22 17:17:57 +0100

    Blog page additions

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -49,3 +49,15 @@ function closeNav() {
 	document.getElementById("toggle").className = document
 			.getElementById("toggle").className.replace('', 'toggle');
 };
+
+function showHide(shID) {
+	if (document.getElementById(shID)) {
+		if (document.getElementById(shID + '-show').style.display != 'none') {
+			document.getElementById(shID + '-show').style.display = 'none';
+			document.getElementById(shID).style.display = 'block';
+		} else {
+			document.getElementById(shID + '-show').style.display = 'inline';
+			document.getElementById(shID).style.display = 'none';
+		}
+	}
+};
