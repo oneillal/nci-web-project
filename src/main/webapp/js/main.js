9ff6c7bc6c1eb25cbd2bbd2c9c03cdc9c3fffbff
Jonas Stein <rsproxx@gmail.com>
2017-04-23 10:41:42 +0100

    added animation for scroling down

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -34,7 +34,8 @@ $(document).ready(
             }
         }
 
-        init();
+	
+	init();
 
     });
 
@@ -61,6 +62,28 @@ function showHide(shID) {
         }
 
     }
-
-
 };
+
+$(function() {
+	$('a[href="#smoothScroll"]').click(function(event){
+	        if (this.hash !== "") {
+
+			// prevent the default action (href) from executing
+			event.preventDefault();
+
+			// this.hash reads the href attribute
+			// in this case hash will be #smoothScroll
+	                var hash = this.hash;
+
+        	        $('html, body').animate({
+				// return the vertical scrollbar position
+				// and assigns it to scrollTop
+                	        scrollTop: $(hash).offset().top - 50
+                        	},
+				// .9 seconds for the transition
+				900
+        	        );
+       		}
+	});
+});
+
